92410aa108365ca06cf2c0e8b49110094927e9e1
Julia Yu <juliayu@gmail.com>
2016-02-22 14:39:43 -0800

    bonus

--- canvas/CanvasViewController.swift
+++ canvas/CanvasViewController.swift
@@ -13,12 +13,22 @@ class CanvasViewController: UIViewController {
     @IBOutlet weak var trayView: UIView!
     
     var trayOriginalCenter: CGPoint?
+    var trayIsOpen = true
+    
+    var trayCenterWhenClosed: CGPoint!
+    var trayCenterWhenOpen: CGPoint!
+    
+    var newlyCreatedFace: UIImageView!
+    var newlyCreatedFaceOriginalCenter: CGPoint?
+    
+    var landedFaceOriginalCenter: CGPoint?
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        
+        self.trayCenterWhenOpen = self.trayView.center
+        self.trayCenterWhenClosed = CGPoint(x: self.view.center.x, y: self.view.frame.size.height + (self.trayView.frame.size.height / 2) - 30)
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,22 +36,124 @@ class CanvasViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    private func toggleTrayView() {
+        var toPoint: CGPoint
+        if self.trayIsOpen {
+            toPoint = self.trayCenterWhenClosed
+        } else {
+            toPoint = self.trayCenterWhenOpen
+        }
+        self.animateTrayCenterToPoint(toPoint)
+        self.trayIsOpen = !self.trayIsOpen
+    }
+    
+    private func animateTrayCenterToPoint(point: CGPoint) {
+        UIView.animateWithDuration(
+            0.35,
+            delay: 0,
+            usingSpringWithDamping: 1,
+            initialSpringVelocity: 0.35,
+            options: .CurveEaseInOut,
+            animations: { () -> Void in
+                self.trayView.center = point
+            },
+            completion: nil
+        )
+    }
+    
+    @IBAction func onTapGesture(tapGestureRecognizer: UITapGestureRecognizer) {
+        self.toggleTrayView()
+    }
+    
     @IBAction func onTrayPanGesture(panGestureRecognizer: UIPanGestureRecognizer) {
-        
-        var point = panGestureRecognizer.locationInView(view)
-        
         // Relative change in (x,y) coordinates from where gesture began.
-        var translation = panGestureRecognizer.translationInView(view)
-        var velocity = panGestureRecognizer.velocityInView(view)
+        let translation = panGestureRecognizer.translationInView(view)
+        let velocity = panGestureRecognizer.velocityInView(view)
         
         if panGestureRecognizer.state == UIGestureRecognizerState.Began {
             trayOriginalCenter = trayView.center
             
         } else if panGestureRecognizer.state == UIGestureRecognizerState.Changed {
-            trayView.center.y = translation.y + trayOriginalCenter!.y
+            let newTrayCenterY = translation.y + trayOriginalCenter!.y
+            trayView.center.y = newTrayCenterY < self.trayCenterWhenOpen.y ? self.trayCenterWhenOpen.y : newTrayCenterY            
+        } else if panGestureRecognizer.state == UIGestureRecognizerState.Ended {
+            if velocity.y < 0 {
+                self.animateTrayCenterToPoint(self.trayCenterWhenOpen)
+                self.trayIsOpen = true
+            } else {
+                self.animateTrayCenterToPoint(self.trayCenterWhenClosed)
+                self.trayIsOpen = false
+            }
+        }
+    }
+    
+    func onNewSmileyPan(panGestureRecognizer: UIPanGestureRecognizer) {
+        let translation = panGestureRecognizer.translationInView(view)
+        
+        let mySmileyView = panGestureRecognizer.view
+        
+        if panGestureRecognizer.state == UIGestureRecognizerState.Began {
+
+            self.landedFaceOriginalCenter = mySmileyView!.center
+            
+        } else if panGestureRecognizer.state == UIGestureRecognizerState.Changed {
+            
+            mySmileyView!.center = CGPoint(
+                x: self.landedFaceOriginalCenter!.x + translation.x,
+                y: self.landedFaceOriginalCenter!.y + translation.y
+            )
             
         } else if panGestureRecognizer.state == UIGestureRecognizerState.Ended {
+
+        }
+    }
+    
+    func onPinchSmiley(pinchGestureRecognizer: UIPinchGestureRecognizer) {
+        let mySmileyView = pinchGestureRecognizer.view
+        
+        if pinchGestureRecognizer.state == UIGestureRecognizerState.Began {
+     
             
+        } else if pinchGestureRecognizer.state == UIGestureRecognizerState.Changed {
+            
+            mySmileyView!.transform = CGAffineTransformMakeScale(pinchGestureRecognizer.scale, pinchGestureRecognizer.scale)
+
+        } else if pinchGestureRecognizer.state == UIGestureRecognizerState.Ended {
+            
+        }
+    }
+    
+    @IBAction func onSmileyPanGesture(panGestureRecognizer: UIPanGestureRecognizer) {
+        // Relative change in (x,y) coordinates from where gesture began.
+        let translation = panGestureRecognizer.translationInView(view)
+        
+        if panGestureRecognizer.state == UIGestureRecognizerState.Began {
+            
+            let imageView = panGestureRecognizer.view as! UIImageView
+            self.newlyCreatedFace = UIImageView(image: imageView.image)
+            self.view.addSubview(self.newlyCreatedFace)
+            self.newlyCreatedFace.center = imageView.center
+            self.newlyCreatedFace.center.y += self.trayView.frame.origin.y
+            self.newlyCreatedFaceOriginalCenter = self.newlyCreatedFace.center
+            
+            let newGestureRecognizer = UIPanGestureRecognizer(target: self, action: "onNewSmileyPan:")
+            self.newlyCreatedFace.addGestureRecognizer(newGestureRecognizer)
+            self.newlyCreatedFace.userInteractionEnabled = true
+            
+            newlyCreatedFace.transform = CGAffineTransformMakeScale(1.5, 1.5)
+            
+            let newPinchRecognizer = UIPinchGestureRecognizer(target: self, action: "onPinchSmiley:")
+            self.newlyCreatedFace.addGestureRecognizer(newPinchRecognizer)
+            
+            
+        } else if panGestureRecognizer.state == UIGestureRecognizerState.Changed {
+            self.newlyCreatedFace.center = CGPoint(
+                x: self.newlyCreatedFaceOriginalCenter!.x + translation.x,
+                y: self.newlyCreatedFaceOriginalCenter!.y + translation.y
+            )
+            
+        } else if panGestureRecognizer.state == UIGestureRecognizerState.Ended {
+            self.newlyCreatedFace.transform = CGAffineTransformMakeScale(1, 1)
         }
     }
 
